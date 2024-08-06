0ef1434dfd1d330c58d764886447db5a19447cea
sangsioco <sangsioco@gmail.com>
2024-08-05 19:28:43 -0700

    adding postgres connection

--- app.py
+++ app.py
@@ -1,6 +1,41 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from marshmallow import fields
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
 
 app = Flask(__name__)
+ma = Marshmallow(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://m16l2_excercise1_sum_post_user:7G7LpYpFSdcI9QZ7obBEBimdg19ZVhC3@dpg-cqoo6nggph6c73f9kdb0-a.oregon-postgres.render.com/m16l2_excercise1_sum_post'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(app, model_class=Base)
+
+class Sum(Base):
+    __tablename__ = "Sum"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    num1: Mapped[int] = mapped_column(db.Integer, nullable=False)
+    num2: Mapped[int] = mapped_column(db.Integer, nullable=False)
+    result: Mapped[int] = mapped_column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Sum {self.id}: {self.num1} + {self.num2} = {self.result}>'
+    
+class SumSchema(ma.Schema):
+    id = fields.Integer()
+    num1 = fields.Integer()
+    num2 = fields.Integer()
+    result = fields.Integer()
+
+sums_schema = SumSchema(many=True)
+
+@app.route('/sum', methods=['GET'])
+def find_all():
+    sums= db.session.execute(db.select(Sum)).scalars()
+    return sums_schema.jsonify(sums), 200
 
 @app.route('/sum', methods=['POST'])
 def sum():
@@ -8,4 +43,14 @@ def sum():
     num1 = data['num1']
     num2 = data['num2']
     result = num1 + num2
+
+    with Session(db.engine) as session:
+        with session.begin():
+            sum_entry = Sum(num1=num1, num2=num2, result=result)
+            session.add(sum_entry)
+
     return jsonify({'result': result})
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
